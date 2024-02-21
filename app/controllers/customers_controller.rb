dd92bbc7c5940d5c42f7135744d08cf0c9020afc
O-Azmi <azmioualid@gmail.com>
2024-02-21 13:33:58 -0600

    routes added, css added, views and partial configured, finishing touches done

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,11 @@
+class CustomersController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+  def alphabetized
+    @customers = Customer.order(:full_name)
+  end
+  def missing_email
+    @customers = Customer.where("email_address IS NULL OR email_address = ''")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'customers/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -6,6 +7,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  root 'customers#index'
+  get '/customers/missing_email', to: 'customers#missing_email', as: 'missing_email_customers'
+  get '/customers/alphabetized', to: 'customers#alphabetized', as: 'alphabetized_customers'
+  
+  resources :customers
+  
   # Defines the root path route ("/")
   # root "posts#index"
   ActiveAdmin.routes(self)
