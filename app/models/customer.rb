b3d1e72a04adf22f0d41b875ba783d21a8dac224
O-Azmi <azmioualid@gmail.com>
2024-02-20 22:20:35 -0600

    ransack gem deleted for testing

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,12 @@
 class Customer < ApplicationRecord
+
+    #Add validation
+    validates :full_name, :phone_number, :notes, presence: true
+
+    has_one_attached :image
+
+
     def self.ransackable_attributes(auth_object = nil)
-      %w[full_name phone_number email_address notes]
+      %w[full_name phone_number email_address notes image]
     end
   end
